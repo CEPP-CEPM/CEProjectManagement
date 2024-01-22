0f7aeac0ea03e81b3c7de8b88309507f24b6c53e
Teerawat Chanvipardilok <82800595+Teerawat36167@users.noreply.github.com>
2024-01-22 23:02:52 +0700

    clear

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='bg-black text-white h-[60px]'>
+            Navbar
+        </div>
+    )
+}
+
+export default Navbar
