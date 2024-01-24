9d8d588080139b599e85115013baa06eea542d6a
Sobitora <web_124@hotmail.com>
2024-01-25 03:54:11 +0700

    update

---
+++ src/app/announcement/page.jsx
@@ -0,0 +1,17 @@
+const Announcement = () => {
+    return (
+        <div className=" p-7">
+            <div className="text-KMITL">
+                Back
+            </div>
+            <div>หัวข้อ</div>
+            <div className=" flex gap-20">
+                <div>Nutchapon Tripat</div>
+                <div>17/1/2024 1:00:01 AM</div>
+            </div>
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente accusamus ipsam nobis dolorem quaerat sunt maiores enim. Cupiditate placeat, tenetur distinctio, id odio sint dolore vitae laudantium, esse nam sunt.</div>
+        </div>
+    )
+}
+
+export default Announcement
