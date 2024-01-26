a8762860b5e11f5a1a7f296bb02d2c66d7e87eaf
Teerawat Chanvipardilok <82800595+Teerawat36167@users.noreply.github.com>
2024-01-26 12:30:19 +0700

    update Modal

--- src/app/proctor/CreatePost.jsx
+++ src/app/proctor/CreatePost.jsx
@@ -1,14 +1,25 @@
 'use client'
 import { useState } from 'react';
-import Backdrop from '@mui/material/Backdrop';
 import Modal from '@mui/material/Modal';
 import Fade from '@mui/material/Fade';
+import './styles.css'
 
 const CreatePost = () => {
 
     const [open, setOpen] = useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+
+    const [type, setType] = useState(true);
+    const [topic, setTopic] = useState('');
+
+    const selectType = (select) => {
+        if (select == 1 && !type) {
+            setType(true);
+        } else if (select == 0 && type) {
+            setType(false);
+        }
+    };
 
     return (
         <div>
@@ -26,9 +37,46 @@ const CreatePost = () => {
                     },
                 }}>
                 <Fade in={open}>
-                    <div className='bg-white left-[25%] top-[5%] absolute w-[50%] h-[90%]'>
+                    <div className='bg-white left-[25%] top-[5%] absolute w-[50%] h-[90%] rounded-md px-[2.5%] py-[2.5%]'>
+                        {/* type */}
+                        <div className='flex mb-[15px]'>
+                            <button
+                            className={`createPost_postForm_typeButton rounded-l-[6px]
+                                ${
+                                    type
+                                    ? 'bg-[#FF6E2F] text-[#FFFFFF]'
+                                    : 'text-[#FF6E2F] hover:bg-[#FF6E2F] hover:text-[#FFFFFF]'
+                                }`}
+                            disabled={type}
+                            onClick={() => selectType(1)}
+                            >
+                            Announcement
+                            </button>
+                            <button
+                            className={`createPost_postForm_typeButton rounded-r-[6px]
+                                ${
+                                    type
+                                    ? 'text-[#FF6E2F] hover:bg-[#FF6E2F] hover:text-[#FFFFFF]'
+                                    : 'bg-[#FF6E2F] text-[#FFFFFF]'
+                                }`}
+                            disabled={!type}
+                            onClick={() => selectType(0)}
+                            >
+                            Assignment
+                            </button>
+                        </div>
 
-                        Hello
+                        {/* Topic */}
+                        <div className=' mb-[15px] '>
+                            <h2 className='text-[15px] font-bold text-[#545F71] mb-2'>หัวข้อ</h2>
+                            <input
+                            type='topic'
+                            className='createPost_postForm_topicInput'
+                            placeholder='หัวข้อ'
+                            onChange={(e) => setTopic(e.target.value)}
+                            value={topic}
+                            />
+                        </div>
                     </div>
                 </Fade>
             </Modal>
