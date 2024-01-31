2438e3078af22a58d033147f6821ac927f67897e
Teerawat Chanvipardilok <82800595+Teerawat36167@users.noreply.github.com>
2024-01-31 21:50:49 +0700

    add sweet alert 2

--- src/app/proctor/dashboard/[assignmentId]/GroupLists.jsx
+++ src/app/proctor/dashboard/[assignmentId]/GroupLists.jsx
@@ -1,5 +1,6 @@
 'use client'
 import { useState } from "react";
+import Swal from 'sweetalert2'
 
 // icon
 import { RiGroupLine } from "react-icons/ri";
@@ -7,6 +8,36 @@ import { RiGroupLine } from "react-icons/ri";
 const GroupLists = () => {
 
     const [students, setStudents] = useState([{id:1, group:'group 1'},{id:2, group:'group 2'},{id:3, group:'group 3'}])
+
+    const handleReject = () => {
+        Swal.fire({
+            title: "Do you want to reject this work?",
+            showDenyButton: true,
+            confirmButtonText: "Reject",
+            confirmButtonColor: "Red",
+            denyButtonText: "cancel",
+            denyButtonColor: "gray"
+        }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire("Reject!", "", "success");
+        }
+        });
+    }
+
+    const handleAccept = () => {
+        Swal.fire({
+            title: "Do you want to accept this work?",
+            showDenyButton: true,
+            confirmButtonText: "confirm",
+            confirmButtonColor: "green",
+            denyButtonText: "cancel",
+            denyButtonColor: "gray"
+        }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire("Accept!", "", "success");
+        }
+        });
+    }
 
     return (
         <div>
@@ -19,8 +50,14 @@ const GroupLists = () => {
                                 <div className="text-[18px]">{data.group}</div>
                             </div>
                             <div className="flex pr-4 mt-3 md:">
-                                <button className="mr-4 px-4 py-2 text-white bg-red-600 rounded-md hover:bg-red-800">Reject</button>
-                                <button className="px-4 py-2 text-white bg-green-600 rounded-md hover:bg-green-800">Accept</button>
+                                <button className="mr-4 px-4 py-2 text-white bg-red-600 rounded-md hover:bg-red-800"
+                                        onClick={() => handleReject()}>
+                                            Reject
+                                </button>
+                                <button className="px-4 py-2 text-white bg-green-600 rounded-md hover:bg-green-800"
+                                        onClick={() => handleAccept()}>
+                                            Accept
+                                </button>
                             </div>
                         </div>
                         <hr/>
