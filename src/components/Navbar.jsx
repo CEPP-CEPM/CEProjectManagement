1da09545680b04a3bb4626d9a1280328ab2620e0
Sobitora <web_124@hotmail.com>
2024-02-01 14:21:32 +0700

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -84,7 +84,7 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
-        <ul className="menu menu-horizontal px-1 flex-nowrap hidden lg:flex text-white">
+        <ul className="menu menu-horizontal px-1 flex-nowrap hidden lg:flex text-black">
           <li>
             <a>Announcement</a>
           </li>
