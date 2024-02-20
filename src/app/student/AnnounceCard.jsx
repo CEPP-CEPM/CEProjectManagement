4e6cf2ce157353b706b3c6eeebe05595a16306a3
Teerawat Chanvipardilok <82800595+Teerawat36167@users.noreply.github.com>
2024-02-20 12:32:48 +0700

    fetch post

--- src/app/student/AnnounceCard.jsx
+++ src/app/student/AnnounceCard.jsx
@@ -1,20 +1,23 @@
+'use client'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
 const AnnounceCard = (props) => {
+    const [announceData, setAnnounceData] = useState([])
 
-    const announceData = [{id: 1, name: 'Alice'},
-    {id: 2, name: 'Bob'},
-    {id: 3, name: 'Carl'},
-    {id: 4, name: 'Bob'},
-    {id: 5, name: 'Bob'},
-    {id: 6, name: 'Bob'},
-    {id: 7, name: 'Bob'},
-    {id: 8, name: 'Bob'},
-    {id: 9, name: 'Bob'},
-    {id: 10, name: 'Alice'},
-    {id: 11, name: 'Bob'},
-    {id: 12, name: 'Carl'},
-    {id: 13, name: 'Carl'},]
-
+    useEffect(() => {
+        const fetchPost = async () => {
+            const announce = await axios.get('http://localhost:3001/announcement')
+                .then((res) => res.data)
+            const assignment = await axios.get('http://localhost:3001/assignment')
+                .then((res) => res.data)
+                setAnnounceData([...announce, ...assignment])
+        }
+        fetchPost()
+    },[])
+    
     const display = announceData.slice(10*(props.page-1), 10*props.page)
+    // console.log(display);
 
     props.setDatacount(announceData.length)
 
@@ -23,8 +26,8 @@ const AnnounceCard = (props) => {
             {display.map((data) => {
                 return (
                     <div key={data.id} className={`flex justify-between bg-[${data.id%2 ? '#FFFFFF' : '#F5F5F5'}] py-[0.6rem] px-5 text-[#595959]`}>
-                        <div>{data.name}</div>
-                        <div className="hidden md:block">16/01/2024</div>
+                        <div>{data.title}</div>
+                        <div className="hidden md:block">{data.createAt.slice(0, 10)}</div>
                     </div>
                 )
             })}
