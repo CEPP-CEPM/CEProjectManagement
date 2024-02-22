e7741f426cb92dc4d63b378811c769f7978371f0
Web <web_124@hotmail.com>
2024-02-23 03:56:14 +0700

    show Advisor

--- src/app/proctor/group/ListAdvisor.jsx
+++ src/app/proctor/group/ListAdvisor.jsx
@@ -1,7 +1,33 @@
-const ListGroup = () =>{
-    return(
-        <div></div>
-    )
-}
+"use client";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-export default ListGroup
+const ListGroup = () => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const fetch = async () => {
+      const advisor = await axios
+        .get(`${process.env.NEXT_PUBLIC_ENDPOINT}/users/Advisor`)
+        .then((res) => res.data);
+      setData(advisor);
+    };
+    fetch();
+  }, []);
+//   console.log(data);
+
+  return (
+    <div className="flex justify-center">
+      {data ? (
+        data.map((d, index) => {
+          return <div>{data[index].name} {data[index].lastname}</div>
+        })
+      ) : (
+        <div></div>
+      )}
+      {/* <div>test</div> */}
+    </div>
+  );
+};
+
+export default ListGroup;
