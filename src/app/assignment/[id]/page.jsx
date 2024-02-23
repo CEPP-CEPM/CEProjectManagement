0e03000fc6a39e80b4efd60f36ef5d2c45a081a4
Web <web_124@hotmail.com>
2024-02-24 04:49:00 +0700

    login get user and token

--- src/app/assignment/[id]/page.jsx
+++ src/app/assignment/[id]/page.jsx
@@ -1,8 +1,9 @@
+'use client';
 import Detail from "@/components/Detail";
 import UploadFile from "./UploadFile";
 
-
 const Assignment = () => {
+
     return (
         <div>
             <Detail head={"head"} name={"Nutchapon Tripat"} date={"17/1/2024 1:00:01 AM"} type={"Assignment"}/>
