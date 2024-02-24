cbd3a86bf807e634b8def2ed86a75108b0089ca0
Web <web_124@hotmail.com>
2024-02-25 02:54:36 +0700

    show member

--- src/app/proctor/group/advisor/[id]/page.jsx
+++ src/app/proctor/group/advisor/[id]/page.jsx
@@ -1,13 +1,11 @@
 'use client';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import ShowGroup from '@/components/ShowGroup';
 
 
 const GroupAdvisor = ({params}) =>{
     const [data, setData] = useState();
-
-    const [show, setShow] = useState(null);
-  const test = 0;
 
   useEffect(() => {
     const fetch = async () => {
@@ -19,26 +17,9 @@ const GroupAdvisor = ({params}) =>{
     fetch();
   }, []);
 
-  console.log(data)
-
     return(
-        <div className='flex flex-col gap-5 w-[500px] h-[400px]'>
-            test
-            {data ? data.map((d, index) => {
-        return (
-          <div>
-            <div className="bg-KMITL text-white px-5 py-3" onClick={() => setShow(index)}>{d.topic}</div>
-            <div
-              className={`${
-                show == index ? "" : "hidden"
-              } px-5 py-3 bg-[#F5F5F5]`}
-            >
-              <div>สมาชิก</div>
-              <div>64011109 นายณัชพล ไตรภัทร์</div>
-            </div>
-          </div>
-        );
-      }) : <div>test</div>}
+        <div className='flex justify-center py-5'>
+            <ShowGroup groupData={data}/>
         </div>
     )
 }
