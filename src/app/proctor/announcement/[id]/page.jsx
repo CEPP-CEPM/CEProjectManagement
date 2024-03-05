1f606a00a731daeff9af384eba705c3a7bab79fe
Teerawat Chanvipardilok <82800595+Teerawat36167@users.noreply.github.com>
2024-03-06 02:30:18 +0700

    update proctor delete post

--- src/app/proctor/announcement/[id]/page.jsx
+++ src/app/proctor/announcement/[id]/page.jsx
@@ -2,8 +2,12 @@
 import Detail from "@/components/Detail";
 import { useState, useEffect } from "react";
 import axios from 'axios'
+import { useRouter } from "next/navigation";
+import Swal from 'sweetalert2'
 
 const Announcement = ({ params }) => {
+
+    const router = useRouter()
 
     const [data, setData] = useState()
     const [files, setFiles] = useState()
@@ -17,10 +21,39 @@ const Announcement = ({ params }) => {
         fetch()
     }, [])
 
+    const handleDelete = async () => {
+        Swal.fire({
+            title: "Do you want to delete post?",
+            showDenyButton: true,
+            confirmButtonText: "Delete",
+            confirmButtonColor: "Red",
+            denyButtonText: "cancel",
+            denyButtonColor: "gray"
+        }).then((result) => {
+        if (result.isConfirmed) {
+            axios.delete(`${process.env.NEXT_PUBLIC_ENDPOINT}/announcement/${params.id}`)
+            .then((res) => {
+                console.log(`Deleted post with ID ${params.id}`);
+            })
+            router.back()
+        }
+        });
+    }
+
     return (
-        <div>
-            {data && <Detail data={data} type='announcement'/>}
-        </div>
+        <>
+            {data && 
+                <>
+                    <Detail data={data} type='announcement'/>
+                    <div className="flex justify-end">
+                        <button className="mr-7 px-4 py-2 text-white bg-red-600 rounded-md hover:bg-red-800"
+                                onClick={() => handleDelete()}>
+                                    Delete
+                        </button>
+                    </div>
+                </>
+            }
+        </>
     )
 }
 
--- src/app/proctor/assignment/[id]/page.jsx
+++ src/app/proctor/assignment/[id]/page.jsx
@@ -2,8 +2,12 @@
 import Detail from "@/components/Detail";
 import axios from 'axios'
 import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Swal from 'sweetalert2'
 
 const Assignment = ({ params }) => {
+
+    const router = useRouter()
 
     const [data, setData] = useState()
 
@@ -16,9 +20,38 @@ const Assignment = ({ params }) => {
         fetch()
     }, [])
 
+    const handleDelete = async () => {
+        Swal.fire({
+            title: "Do you want to delete post?",
+            showDenyButton: true,
+            confirmButtonText: "Delete",
+            confirmButtonColor: "Red",
+            denyButtonText: "cancel",
+            denyButtonColor: "gray"
+        }).then((result) => {
+        if (result.isConfirmed) {
+            axios.delete(`${process.env.NEXT_PUBLIC_ENDPOINT}/assignment/${params.id}`)
+            .then((res) => {
+                console.log(`Deleted post with ID ${params.id}`);
+            })
+            router.back()
+        }
+        });
+    }
+
     return (
         <>
-            {data && <Detail data={data} type='Assignment'/>}
+            {data && 
+            <>
+                <Detail data={data} type='Assignment'/>
+                <div className="flex justify-end">
+                    <button className="mr-7 px-4 py-2 text-white bg-red-600 rounded-md hover:bg-red-800"
+                            onClick={() => handleDelete()}>
+                                Delete
+                    </button>
+                </div>
+            </>
+            }
         </>
     )
 }
