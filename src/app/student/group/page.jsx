00c8b5cb4d53f0484119dc63317ff39817fe08e8
Web <web_124@hotmail.com>
2024-03-16 23:59:19 +0700

    update accept group

--- src/app/student/group/page.jsx
+++ src/app/student/group/page.jsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react";
 const Group = () => {
   const session = useSession();
   const [token, setToken] = useState("");
-  const [group, setGroup] = useState()
+  const [group, setGroup] = useState();
 
   useEffect(() => {
     const fetch = async () => {
@@ -17,8 +17,9 @@ const Group = () => {
           },
         })
         .then((res) => {
-            console.log(res.data);
-            setGroup(res.data)});
+          console.log(res.data);
+          setGroup(res.data);
+        });
     };
     if (session.status == "authenticated") {
       setToken(session.data.accessToken);
@@ -26,16 +27,38 @@ const Group = () => {
     }
   }, [session.status]);
 
+  const acceptgroup =  async() =>{
+    await axios.patch(`${process.env.NEXT_PUBLIC_ENDPOINT}/group/student/join`,{}, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => console.log(res.data)
+
+      )
+  }
+
   return (
     <div className="flex flex-col items-center">
       <div>
-        <div>My Group</div>
-        <div className="w-[500px] h-[400px] border-2">
-          <div>{group?.topic}</div>
-          <div>{group?.Users.name} {group?.Users.lastname}</div>
-          {group?.UserGroups.map((member)=>{
-                return<div key={member.id}>{member.Users.name} {member.Users.lastname}</div>
+        <div className="text-KMITL font-bold text-[40px] my-10">My Group</div>
+        <div className="w-[500px] bg-[#F5F3F3] shadow-lg rounded-lg px-5 py-3">
+          <div>หัวข้อโครงงาน : {group?.topic}</div>
+          <div>
+            อาจารย์ที่ปรึกษา : {group?.Users.name} {group?.Users.lastname}
+          </div>
+          <div>รายชื่อสมาชิก : </div>
+          {group?.UserGroups.map((member) => {
+            return (
+              <div key={member.id}>
+                {member.Users.name} {member.Users.lastname}
+              </div>
+            );
           })}
+          <div>
+            <button className="btn btn-error">Reject</button>
+            <button className="btn btn-success" onClick={acceptgroup}>Accept</button>
+          </div>
         </div>
       </div>
     </div>
